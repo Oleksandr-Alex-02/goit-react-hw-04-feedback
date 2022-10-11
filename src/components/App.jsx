002208664714d168e063c1b939605bcfab94bbd4
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-10-11 16:54:13 +0300

    g

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,30 @@
+import React from 'react';
 import Button from "./button/Button";
 
-export const App = () => {
-  return (
+function App() {
+  // state = {
+  //   good: 0,
+  //   neutral: 0,
+  //   bad: 0
+  // }
+
+  // render() {
+  // const { good, neutral, bad } = this.state
+
+  return (<div>
     <div>
+      <h1>Please leave feedback</h1>
       <Button />
     </div>
-  );
-};
+    <div>
+      <h2>Statistics</h2>
+      <p>Bad: </p>
+      <p>Neutral: </p>
+      <p>Good: </p>
+    </div>
+  </div >
+  )
+}
+// }
 
+export default App;
--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,37 +1,10 @@
-import React, { Component } from "react";
-
-class Button extends Component() {
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0
-    }
-
-    onLeaveFeedback = option => {
-        this.setState(prevState => ({ [option]: prevState[option] + 1 }));
-    };
-
-    render() {
-        // const { good, neutral, bad } = this.state;
-        // const total = good + neutral + bad;
-        // const positivePercentage = Number.parseInt((good / total) * 100);
-
-        return (<div>
-            <div>
-                <h1>Please leave feedback</h1>
-                <button onClick={() => this.onLeaveFeedback}>Good</button>
-                <button onClick={() => this.onLeaveFeedback}>Neutral</button>
-                <button onClick={() => this.onLeaveFeedback}>Bad</button>
-            </div>
-            <div>
-                <h2>Statistics</h2>
-                <p>Bad:</p>
-                <p>Neutral:</p>
-                <p>Good:</p>
-            </div>
-        </div >
-        )
-    }
+function Button() {
+    return (<div>
+        <button onClick={() => { console.log('123') }}>Good</button>
+        <button>Neutral</button>
+        <button>Bad</button>
+    </div>
+    )
 }
 
 export default Button;
